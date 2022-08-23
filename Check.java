6208d8431b2f311f077bcd4f4dd58a1abca81b20
moumita012 <110465896+moumita012@users.noreply.github.com>
2022-08-23 14:48:37 +0530

    Add files via upload

---
+++ Check.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Check {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number");
+		int a= sc.nextInt();
+		if(a>0) {
+			System.out.println("The number is positive");
+		}else if(a<0) {
+			System.out.println("The number is negetive");
+		} else {
+			System.out.println("the number is zero");
+		}
+	}
+
+}
